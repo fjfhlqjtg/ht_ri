a4e4008c9694c4f0919c0970bfc026f85823e90f
fjfhlqjtg <fjfhlqjtg@gmail.com>
2013-01-03 21:22:55 +0800

    增加 分出合同计算任务计划
    Signed-off-by: fjfhlqjtg <fjfhlqjtg@gmail.com>

---
+++ src/main/java/com/fjfhlqjtg/outcontacc/OutContAccBs.java
@@ -0,0 +1,19 @@
+package com.fjfhlqjtg.outcontacc;
+
+public class OutContAccBs {
+
+	/**
+	 * 分出合同计算服务
+	 * @author fjfhlqjtg
+	 * @param xmlStr
+	 * @return
+	 */
+	public String outcontacc(String xmlStr){
+		String msg=null;
+		//TODO 1、校验传入xml字符串是否为空或规范
+		//TODO 2、转换xml字符串为JavaBean
+		//TODO 3、校验JavaBean和字段是否符合业务条件
+		//TODO 4、分别保存日志信息、基本信息、离线任务信息和缴费计划信息
+		return msg;
+	}
+}
