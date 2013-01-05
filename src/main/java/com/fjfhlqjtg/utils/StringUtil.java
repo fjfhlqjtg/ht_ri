b88faa7ed659fa645b6c5db9e8cbfe26502a51a4
fjfhlqjtg <fjfhlqjtg@gmail.com>
2013-01-05 09:46:16 +0800

    增加注释
    Signed-off-by: fjfhlqjtg <fjfhlqjtg@gmail.com>

--- src/main/java/com/fjfhlqjtg/utils/StringUtil.java
+++ src/main/java/com/fjfhlqjtg/utils/StringUtil.java
@@ -2,8 +2,19 @@ package com.fjfhlqjtg.utils;
 
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * 字符串工具类
+ * @author fjfhlqjtg
+ *
+ */
 public class StringUtil {
 
+	/**
+	 * <p>判断字符串是否为空</p>
+	 * @author fjfhlqjtg
+	 * @param str 要校验的字符串
+	 * @return {@code true} 如果字符串为空或为null
+	 */
 	public static boolean isNull(String str) {
 		if (StringUtils.isEmpty(str) || StringUtils.isBlank(str))
 			return true;
@@ -11,6 +22,12 @@ public class StringUtil {
 			return false;
 	}
 
+	/**
+	 * <p>判断字符串是否不为空</p>
+	 * @author fjfhlqjtg
+	 * @param str 要校验的字符串
+	 * @return {@code true} 如果字符串不为空并且不为null
+	 */
 	public static boolean isNotNull(String str) {
 		if (isNull(str))
 			return false;
