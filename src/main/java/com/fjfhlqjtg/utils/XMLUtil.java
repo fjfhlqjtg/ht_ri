6d3a28da36147119d6d8a2f1448ec9655d1a714f
fjfhlqjtg <fjfhlqjtg@gmail.com>
2013-01-05 11:32:07 +0800

    增加 xml转换JavaBean工具类 增加 转换分出合同转换
    Signed-off-by: fjfhlqjtg <fjfhlqjtg@gmail.com>

---
+++ src/main/java/com/fjfhlqjtg/utils/XMLUtil.java
@@ -0,0 +1,148 @@
+package com.fjfhlqjtg.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.fjfhlqjtg.outcontacc.vo.OutContAccVo;
+import com.fjfhlqjtg.outcontacc.vo.PlyInfoVo;
+import com.fjfhlqjtg.outcontacc.vo.PlyPayPlanVo;
+import com.fjfhlqjtg.outcontacc.vo.TaskInfoVo;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.basic.DateConverter;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+/**
+ * xml字符串转换JavaBean工具类
+ * 
+ * @author fjfhlqjtg
+ * 
+ */
+public class XMLUtil {
+
+	/**
+	 * <p>
+	 * 将入参xml字符串转换为JavaBean
+	 * </p>
+	 * 
+	 * @param serviceType
+	 *            服务名称
+	 * @param xmlStr
+	 *            入参xml字符串
+	 * @return
+	 * @return
+	 */
+	public static Object parseXml2JavaBean(String serviceType, String xmlStr)
+			throws Exception {
+		Object obj = null;
+		if (StringUtils.equalsIgnoreCase("OUTCONTACC", xmlStr)) {
+			obj = parseXml2OutContAcc(xmlStr);
+		} else if (StringUtils.equalsIgnoreCase("OUTFACACC", xmlStr)) {
+
+		} else if (StringUtils.equalsIgnoreCase("INFACACC", xmlStr)) {
+
+		}
+		return obj;
+	}
+
+	/**
+	 * 将入参xml字符串转换为分出合同JavaBean
+	 * 
+	 * @param xmlStr
+	 *            入参xml字符串
+	 * @return <p>分出合同JavaBean</p><see>com.fjfhlqjtg.outcontacc.vo.OutContAccVo</see>
+	 */
+	private static Object parseXml2OutContAcc(String xmlStr) {
+		OutContAccVo vo = null;
+		if (StringUtil.isNotNull(xmlStr)) {
+			XStream xstream = new XStream(new DomDriver());
+			xstream.registerConverter(new DateConverter("yyyy-MM-dd HH:mm:ss",
+					new String[] { "YYYY-MM-DD", "yyyy-MM-dd HH:mm:ss.S" }));
+			xstream.alias("OUTCONTACC", OutContAccVo.class);
+			xstream.aliasField("SERVICE_TYPE", OutContAccVo.class,
+					"serviceType");
+			xstream.aliasField("TRANSACTION_TYPE", OutContAccVo.class,
+					"operateType");
+
+			xstream.aliasField("PLY_INFO", OutContAccVo.class, "plyInfoVo");
+			xstream.alias("PLY_INFO", PlyInfoVo.class);
+			xstream.aliasField("CREATEDATETIME", PlyInfoVo.class, "crtTime");
+			xstream.aliasField("CREATEUSERCODE", PlyInfoVo.class, "crtCode");
+			xstream.aliasField("CHANGEDATETIME", PlyInfoVo.class, "updTime");
+			xstream.aliasField("CHANGEUSERCODE", PlyInfoVo.class, "updCode");
+			xstream.aliasField("PROPOSALNO", PlyInfoVo.class, "plyAppNo");
+			xstream.aliasField("POLICYNO", PlyInfoVo.class, "plyNo");
+			xstream.aliasField("EDRAPPNO", PlyInfoVo.class, "edrAppNo");
+			xstream.aliasField("ENDORSENO", PlyInfoVo.class, "edrNo");
+			xstream.aliasField("DOCUMENTNO", PlyInfoVo.class, "docNo");
+			xstream.aliasField("DOCUMENTNTYPE", PlyInfoVo.class, "docType");
+			xstream.aliasField("PROPOSALSERIALNO", PlyInfoVo.class, "serialNo");
+			xstream.aliasField("PRODUCTNODECODE", PlyInfoVo.class, "insuCode");
+			xstream.aliasField("DEPTCODE", PlyInfoVo.class, "deptNo");
+			xstream.aliasField("INSUREDNAME", PlyInfoVo.class, "insuName");
+			xstream.aliasField("AGENTCODE", PlyInfoVo.class, "agentCode");
+			xstream.aliasField("AGENTCOMMPRT", PlyInfoVo.class, "agentComPrt");
+			xstream.aliasField("STARTDATE", PlyInfoVo.class, "insuBegTime");
+			xstream.aliasField("ENDDATE", PlyInfoVo.class, "insuEndTime");
+			xstream.aliasField("PAYSTART", PlyInfoVo.class, "payBegTime");
+			xstream.aliasField("PAYEND", PlyInfoVo.class, "payEndTime");
+			xstream.aliasField("PAYMENTMODE", PlyInfoVo.class, "payMode");
+			xstream.aliasField("INSURANCEVALUECURRENCY", PlyInfoVo.class,
+					"insuCurr");
+			xstream.aliasField("INSURANCEVALUE", PlyInfoVo.class, "insuAmt");
+			xstream.aliasField("INSURANCEVALUEVAR", PlyInfoVo.class,
+					"insuAmtVar");
+			xstream.aliasField("PREMIUMCURRENCY", PlyInfoVo.class, "prmCurr");
+			xstream.aliasField("PREMIUM", PlyInfoVo.class, "prmAmt");
+			xstream.aliasField("PREMIUMVAR", PlyInfoVo.class, "prmAmtVar");
+			xstream.aliasField("DEVELOPBUSINESSAMT", PlyInfoVo.class, "showAmt");
+			xstream.aliasField("DEVELOPBUSINESSVAR", PlyInfoVo.class,
+					"showAmtVar");
+			xstream.aliasField("COMMISSIONAMT", PlyInfoVo.class, "commisonAmt");
+			xstream.aliasField("COMMISSIONVAR", PlyInfoVo.class,
+					"commisonAmtVar");
+			xstream.aliasField("BROKERAMT", PlyInfoVo.class, "brokerAmt");
+			xstream.aliasField("BROKERVAR", PlyInfoVo.class, "brokerAmtVar");
+			xstream.aliasField("ISSUEDATE", PlyInfoVo.class, "insuTime");
+			xstream.aliasField("PAYBEFOREISSUEDATE", PlyInfoVo.class,
+					"payBeforeInsuTime");
+			xstream.aliasField("MAXDATE", PlyInfoVo.class, "maxTime");
+			xstream.aliasField("REMARK", PlyInfoVo.class, "remark");
+			xstream.aliasField("SALESYS", PlyInfoVo.class, "saleSys");
+			xstream.aliasField("PERSISTMARK", PlyInfoVo.class, "persistMark");
+			xstream.aliasField("EXCLUDE", PlyInfoVo.class, "exclude");
+			xstream.aliasField("RISKLEVEL", PlyInfoVo.class, "riskLev");
+
+			xstream.aliasField("TASK_INFO", OutContAccVo.class, "taskInfoVo");
+			xstream.alias("TASK_INFO", TaskInfoVo.class);
+			xstream.aliasField("CREATEDATETIME", TaskInfoVo.class, "crtTime");
+			xstream.aliasField("CREATEUSERCODE", TaskInfoVo.class, "crtCode");
+			xstream.aliasField("CHANGEDATETIME", TaskInfoVo.class, "updTime");
+			xstream.aliasField("CHANGEUSERCODE", TaskInfoVo.class, "updCode");
+			xstream.aliasField("PROPOSALNO", TaskInfoVo.class, "plyAppNo");
+			xstream.aliasField("POLICYNO", TaskInfoVo.class, "plyNo");
+			xstream.aliasField("APPLYNO", TaskInfoVo.class, "edrAppNo");
+			xstream.aliasField("ENDORSENO", TaskInfoVo.class, "edrNo");
+			xstream.aliasField("IFISSUED", TaskInfoVo.class, "isIssued");
+			xstream.aliasField("INSURANCEVALUE", TaskInfoVo.class, "insuAmt");
+			xstream.aliasField("PREMIUM", TaskInfoVo.class, "prmAmt");
+			xstream.aliasField("TASKSTARTDATE", TaskInfoVo.class, "startTime");
+			xstream.aliasField("DEPTCODE", TaskInfoVo.class, "dptCode");
+
+			xstream.aliasField("PLY_PAY_PLAN_INFO_LIST", OutContAccVo.class,
+					"planList");
+			xstream.alias("PLY_PAY_PLAN_INFO", PlyPayPlanVo.class);
+			xstream.aliasField("PROPOSALNO", PlyPayPlanVo.class, "plyAppNo");
+			xstream.aliasField("POLICYNO", PlyPayPlanVo.class, "plyNo");
+			xstream.aliasField("APPLYNO", PlyPayPlanVo.class, "edrAppNo");
+			xstream.aliasField("ENDORSENO", PlyPayPlanVo.class, "edrNo");
+			xstream.aliasField("PAYSEQUENCE", PlyPayPlanVo.class, "paySeq");
+			xstream.aliasField("PAYSTARTDATE", PlyPayPlanVo.class, "payBegTime");
+			xstream.aliasField("PAYENDDATE", PlyPayPlanVo.class, "payEndTime");
+			xstream.aliasField("PREMIUM", PlyPayPlanVo.class, "prmAmt");
+			xstream.aliasField("CURRENCY", PlyPayPlanVo.class, "curr");
+			xstream.aliasField("STATE", PlyPayPlanVo.class, "state");
+			vo = (OutContAccVo) xstream.fromXML(xmlStr);
+		}
+		return vo;
+	}
+
+}
