a0c86f8157145638377b3a7feccaabad72c3fd9e
fjfhlqjtg <fjfhlqjtg@gmail.com>
2013-01-05 17:16:00 +0800

    增加 保存框架
    Signed-off-by: fjfhlqjtg <fjfhlqjtg@gmail.com>

--- src/main/java/com/fjfhlqjtg/outcontacc/bs/OutContAccBs.java
+++ src/main/java/com/fjfhlqjtg/outcontacc/bs/OutContAccBs.java
@@ -1,37 +1,110 @@
 package com.fjfhlqjtg.outcontacc.bs;
 
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fjfhlqjtg.outcontacc.dao.OutContAccDao;
 import com.fjfhlqjtg.outcontacc.vo.OutContAccVo;
+import com.fjfhlqjtg.outcontacc.vo.PlyInfoVo;
+import com.fjfhlqjtg.outcontacc.vo.PlyPayPlanVo;
+import com.fjfhlqjtg.outcontacc.vo.TaskInfoVo;
 import com.fjfhlqjtg.utils.MsgUtil;
 import com.fjfhlqjtg.utils.StringUtil;
 import com.fjfhlqjtg.utils.XMLUtil;
 
 public class OutContAccBs {
-
+	private static Logger log = LoggerFactory.getLogger(OutContAccBs.class);
+	@Autowired
+	private OutContAccDao dao;
 	/**
 	 * 分出合同计算服务
+	 * 
 	 * @author fjfhlqjtg
 	 * @param xmlStr
 	 * @return
 	 */
-	public String outcontacc(String xmlStr){
-		String msg=null;
+	public String outcontacc(String xmlStr) {
+		String msg = null;
 		// 1、校验传入xml字符串是否为空或规范
-		if(StringUtil.isNotNull(xmlStr)){
-			//TODO 2、转换xml字符串为JavaBean
-			OutContAccVo vo= null;
-			Object obj=null;
+		if (StringUtil.isNotNull(xmlStr)) {
+			// 2、转换xml字符串为JavaBean
 			try {
-				obj = XMLUtil.parseXml2JavaBean("OUTCONTACC",xmlStr);
+				OutContAccVo vo = null;
+				Object obj = XMLUtil.parseXml2JavaBean("OUTCONTACC", xmlStr);
+				if (obj != null)
+					vo = (OutContAccVo) obj;
+				// TODO 3、校验JavaBean和字段是否符合业务条件
+				ValidatorFactory factory = Validation
+						.buildDefaultValidatorFactory();
+				Validator validator = factory.getValidator();
+				Set<ConstraintViolation<OutContAccVo>> constraintValidator = validator
+						.validate(vo);
+				log.error(constraintValidator.iterator().next().getMessage());
+				// TODO 4、分别保存日志信息、基本信息、离线任务信息和缴费计划信息
+				save(vo);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			if(obj!=null)vo=(OutContAccVo) obj;
-			
-		}else {
-			msg=MsgUtil.buildReturnMsg("OUTCONTACC","4","-4001","入参xml为空");
+		} else {
+			msg = MsgUtil.buildReturnMsg("OUTCONTACC", "4", "-4001", "入参xml为空");
 		}
-		//TODO 3、校验JavaBean和字段是否符合业务条件
-		//TODO 4、分别保存日志信息、基本信息、离线任务信息和缴费计划信息
 		return msg;
+	}
+
+	/**
+	 * 保存xml信息到数据库
+	 * 
+	 * @param vo
+	 */
+	private void save(OutContAccVo vo) {
+		if (vo != null) {
+			PlyInfoVo baseVo = vo.getPlyVo();
+			TaskInfoVo taskVo = vo.getTaskVo();
+			List<PlyPayPlanVo> planList = vo.getPlanList();
+			saveBaseInfo(baseVo);
+			saveTaskInfo(taskVo);
+			savePayPlan(planList);
+		}
+	}
+
+	/**
+	 * 保存付款计划信息
+	 * 
+	 * @param planList
+	 *            付款计划
+	 */
+	private void savePayPlan(List<PlyPayPlanVo> planList) {
+		for(PlyPayPlanVo vo:planList){
+			dao.savePayPlan(vo);
+		}
+	}
+
+	/**
+	 * 保存离线任务信息
+	 * 
+	 * @param taskVo
+	 *            离线任务
+	 */
+	private void saveTaskInfo(TaskInfoVo taskVo) {
+		dao.saveTaskInfo(taskVo);
+	}
+
+	/**
+	 * 保存基本信息
+	 * 
+	 * @param baseVo
+	 *            基本信息
+	 */
+	private void saveBaseInfo(PlyInfoVo baseVo) {
+		dao.saveBaseInfo(baseVo);
 	}
 }
