3790dc631dd86ab35415a2899a59aaeff51a5e90
fjfhlqjtg <fjfhlqjtg@gmail.com>
2013-01-21 15:45:29 +0800

    增加 保存基本信息和费用信息
    Signed-off-by: fjfhlqjtg <fjfhlqjtg@gmail.com>

--- src/main/java/com/fjfhlqjtg/infacacc/bs/InFacAccBs.java
+++ src/main/java/com/fjfhlqjtg/infacacc/bs/InFacAccBs.java
@@ -17,6 +17,7 @@ import com.fjfhlqjtg.infacacc.dao.InFacAccDao;
 import com.fjfhlqjtg.infacacc.vo.BaseVo;
 import com.fjfhlqjtg.infacacc.vo.ExpenseVo;
 import com.fjfhlqjtg.infacacc.vo.InFacAccVo;
+import com.fjfhlqjtg.infacacc.vo.PlanVo;
 import com.fjfhlqjtg.log.bs.LogBs;
 import com.fjfhlqjtg.utils.MsgUtil;
 import com.fjfhlqjtg.utils.StringUtil;
@@ -72,10 +73,10 @@ public class InFacAccBs {
 				log.error(e.getMessage());
 				msg = MsgUtil.buildReturnMsg("INFACACC", "1", "-1003",
 						"入参校验失败.");
-			} catch(SQLException e){
+			} catch (SQLException e) {
 				log.error(e.getMessage());
 				msg = MsgUtil.buildReturnMsg("INFACACC", "1", "-1004",
-						"数据保存失败."+e.getMessage());
+						"数据保存失败." + e.getMessage());
 			} catch (Exception e) {
 				log.error(e.getMessage());
 				msg = MsgUtil.buildReturnMsg("INFACACC", "1", "-1900", "其它错误."
@@ -91,22 +92,46 @@ public class InFacAccBs {
 	 * 保存入参信息
 	 * 
 	 * @param vo
+	 *            入参XML转换的JavaBean
+	 *            ，详细见<see>com.fjfhlqjtg.infacacc.vo.InFacAccVo</ see>
 	 */
 	private void saveInfo(InFacAccVo vo) throws Exception {
 		// 3.1、保存基本信息
 		BaseVo baseVo = vo.getBaseVo();
 		saveBaseInfo(baseVo);
-		// TODO 3.2、保存费用信息
+		// 3.2、保存费用信息
 		List<ExpenseVo> expenList = vo.getExpenseList();
 		saveExpenList(expenList);
 		// TODO 3.3、保存账单信息
-		
+		List<PlanVo> planList = vo.getPlanList();
+		saveMaAndSub(baseVo, expenList, planList);
+	}
+
+	/**
+	 * 保存主子表信息
+	 * 
+	 * @param baseVo
+	 *            基本信息JavaBean,详细见<see>com.fjfhlqjtg.infacacc.vo.BaseVo</see>
+	 * @param expenList
+	 *            费用信息JavaBean集合,详细见<see>com.fjfhlqjtg.infacacc.vo.ExpenseVo</
+	 *            see>
+	 * @param planList
+	 *            付款计划JavaBean集合,详细见<see>com.fjfhlqjtg.infacacc.vo.PlanVo</see>
+	 */
+	private void saveMaAndSub(BaseVo baseVo, List<ExpenseVo> expenList,
+			List<PlanVo> planList) {
+		if(baseVo!=null&&expenList!=null&&planList!=null){
+			
+		}
+
 	}
 
 	/**
 	 * 保存分入临分费用信息
 	 * 
 	 * @param expenList
+	 *            费用信息JavaBean集合,详细见<see>com.fjfhlqjtg.infacacc.vo.ExpenseVo</
+	 *            see>
 	 */
 	private void saveExpenList(List<ExpenseVo> expenList) throws Exception {
 		if (expenList != null) {
@@ -122,6 +147,7 @@ public class InFacAccBs {
 	 * 保存分入临分基本信息
 	 * 
 	 * @param baseVo
+	 *            基本信息JavaBean，详细见<see>com.fjfhlqjtg.infacacc.vo.BaseVo</see>
 	 */
 	private void saveBaseInfo(BaseVo baseVo) throws Exception {
 		if (baseVo != null) {
